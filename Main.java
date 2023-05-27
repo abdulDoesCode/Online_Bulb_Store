7e9510933aa7d8e5b793c51ebbe0b06bcfad72b5
abdelrahman nashaat <abdulrahman.nashaat@gmail.com>
2023-05-27 07:17:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
         // Instantiate the data structure class (LinkedList or BinarySearchTree)
-        // LinkedList bulbList = new LinkedList(0, null, 0, 0);
-        BinarySearchTree bulbtree = new BinarySearchTree(0,null,0,0);
+        LinkedList bulbList = new LinkedList(0, null, 0, 0);
+        BinarySearchTree bulbtree = new BinarySearchTree(null,null);
         
         //creating bulbs 
     	Random random = new Random();
@@ -24,7 +24,7 @@ public class Main {
 		                 "Cree LED"};
 		
 	for(int i = 0;i<10;i++) {
-		bulbtree.insert(new Bulb(random.nextInt(1000), name[i], random.nextInt(20), random.nextInt(10)));
+		bulbList.addBulb(new Bulb(random.nextInt(1000), name[i], random.nextInt(20), random.nextInt(10)));
 		
 	}
         // Display menu and handle user input
@@ -35,7 +35,8 @@ public class Main {
             System.out.println("1. Delete a bulb");
             System.out.println("2. Search for a bulb");
             System.out.println("3. Display all bulbs");
-            System.out.println("4. Exit");
+            System.out.println("4. Select a bulb");
+            System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
             scanner.nextLine();
@@ -48,7 +49,7 @@ public class Main {
 
                 // Delete the bulb from the data structure
                 //bulbList.delete(deleteId);
-                bulbtree.delete(deleteId);
+                bulbList.delete(deleteId);
                 break;
 
             case 2:
@@ -58,17 +59,34 @@ public class Main {
 
                 // Search for the bulb in the data structure
                // or  bulbList.search(searchId);
-                bulbtree.search(searchId);
+                bulbList.searchBulb(searchId);
+                
                 break;
 
             case 3:
                 // Display all bulbs in the data structure
                 //bulbList.display();
-                bulbtree.traverse();
+                bulbList.displayBulbs();
                 break;
 
             case 4:
-                System.out.println("Exiting the program.");
+                
+                System.out.println("enter Customer name: ");
+                String CustomerName = scanner.nextLine();
+                System.out.println("enter Customer Address: ");
+                String CustomerAddress = scanner.nextLine();
+            	bulbtree.insert(new Customer(CustomerName,CustomerAddress));
+                System.out.println("enter ID a bulb to purchase: ");
+                int bulbID = scanner.nextInt();
+                	bulbtree.traverse();
+                	bulbList.searchBulb(bulbID);
+                System.out.println("On the way to: " + CustomerName + ", with Address: " +CustomerAddress);
+                System.out.println("Thank you for shopping with us");
+                	
+                break;
+            
+            case 5:
+                System.out.println("Have a great day");
                 break;
 
             default:
@@ -77,7 +95,7 @@ public class Main {
         }
 
         System.out.println(); // Print a blank line for readability
-    } while (choice != 4);
+    } while (choice != 5);
 
     scanner.close();
 }
